f786259b13cb1f763d74fc779fc676647c461b55
Riqueal <queirozricardo@gmail.com>
2019-09-02 18:41:48 -0300

    Configuração de recarga III

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -9,10 +9,23 @@ class Balance extends Model
     //
     public $timestamps = false;
 
-    public function deposit($value)
+    public function deposit(float $value) : Array
     {
 
-        dd($value);
+        $this->amount += number_format($value, 2, '.', '');
+
+        $deposit = $this->save();
+
+        if($deposit)
+            return [
+                'success'   => true,
+                'message'   => 'Sucesso ao recarregar!'
+            ];
+        
+        return [
+            'success'   => false,
+            'message'   => 'Falha ao recarregar!'
+        ];
 
     }
 }
