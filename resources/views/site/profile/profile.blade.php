c09a652c01d1f433fa69450a16c9b2e781443141
Riqueal <queirozricardo@gmail.com>
2019-09-04 15:23:36 -0300

    Atualizando perfil II

--- resources/views/site/profile/profile.blade.php
+++ resources/views/site/profile/profile.blade.php
@@ -5,6 +5,17 @@
 @section('content')
 
 <h1>meu Perfil</h1>
+@if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
 
 <form action="{{ route('profile.update')}}" method="POST">
     {!! csrf_field() !!}
